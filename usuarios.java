4d60f734704aa2b2b79adb650f2374bb01fca7da
gusworkout <71235496+gusworkout@users.noreply.github.com>
2024-11-02 19:40:43 -0500

    Create usuarios.java

---
+++ usuarios.java
@@ -0,0 +1,29 @@
+/**
+ *
+ * @author Gustavo
+ */
+public class User {
+    String user;
+        String password;
+        
+        
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+       public void insertarUsuarios(){
+           
+       }
+}
